93875e7b8fee77838473c41e3ab148c4113f57a4
bigmanstan <theonlyrealemailid@gmail.com>
2017-01-30 22:23:50 +0530

    uses of 'int main" and "void main".

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -6,7 +6,21 @@
  * 
  * Print "Hello, World!" to the console
  */
+
+/*
+*since this program does not return a 
+*value ,therefore we can use "void"
+*instead of "int"
+*E.G.
+*
+*
+#include<stdio.h>
+void main(){
+	printf("hello,world");
+}
+
 int main(void){
     printf("Hello, World!\n");
     return 0;
 }
+*/
